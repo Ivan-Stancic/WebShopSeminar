e199eafeab66f2b51c1353cd30e4e0e4cc5d2e3e
Ivan-Stancic <ivanstancic983@gmail.com>
2022-07-28 14:51:34 +0200

    Add project files.

---
+++ Models/Dbo/Address.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebShopSeminar.Models.Dbo
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
---
+++ Services/Implementation/IdentityService.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebShopSeminar.Models;
+using WebShopSeminar.Models.Dbo;
+using WebShopSeminar.Services.Interface;
+
+namespace WebShopSeminar.Services.Implementation
+{
+    public class IdentityService : IIdentityService
+    {
+        private RoleManager<IdentityRole> roleManager;
+        private UserManager<ApplicationUser> userManager;
+
+        //Prilikom pokretanja aplikacije imamo spremne role u sustavu, u konstruktoru nema async dodaje se wait.
+        public IdentityService(IServiceScopeFactory scopeFactory)
+        {
+            using (var scope = scopeFactory.CreateScope())
+            {
+                this.userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                this.roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                CreateRoleAsync(Roles.Admin).Wait();
+                CreateRoleAsync(Roles.Editor).Wait();
+                CreateRoleAsync(Roles.BasicUser).Wait();
+
+                //Kreiranje Admina
+                CreateUserAsync(new ApplicationUser
+                {
+                    Firstname = "Ivan",
+                    Lastname = "Stančić",
+                    Email = "ivan@sova.hr",
+                    UserName = "Ivan",
+                    DOB = DateTime.Now.AddYears(-35),
+                    Address = new List<Address>
+                    {
+                        new Address
+                        {
+                            City = "Zagreb Grad",
+                            Street = "Maximirska",
+                        }
+                    }
+                }, "Pa$$w0rd", Roles.Admin).Wait();
+            }
+        }
+        //Metoda kreiranja rola
+        public async Task CreateRoleAsync(string role)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole
+                {
+                    Name = role
+                });
+            }
+        }
+
+        //Kreiranje usera
+        public async Task CreateUserAsync(ApplicationUser user, string password, string role)
+        {
+            //Provjera ima li korisnika sa istim mailom u bazi
+            var find = await userManager.FindByEmailAsync(user.Email);
+            if (find != null)
+            {
+                return;
+            }
+            //Potvrda maila za account
+            user.EmailConfirmed = true;
+            var createdUser = await userManager.CreateAsync(user, password);
+            //Provjera dali je korisnik uspiješno dodan
+            if (createdUser.Succeeded)
+            {
+                //Dodavanje u rolu
+                var userAddedToRole = await userManager.AddToRoleAsync(user, role);
+
+                //Ako korisnik nema rolu baca grešku
+                if (!userAddedToRole.Succeeded)
+                {
+                    throw new Exception("Korisnik nema rolu!");
+                }
+            }
+        }
+    }
+}
