8d4d673ee0620eec57316929a3a2ac8a9540e0d1
Ivan-Stancic <ivanstancic983@gmail.com>
2022-08-08 20:05:13 +0200

    user

---
+++ Data/Migrations/20220808180220_userNewMigration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebShopSeminar.Data.Migrations
+{
+    public partial class userNewMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "AspNetUsers");
+        }
+    }
+}
